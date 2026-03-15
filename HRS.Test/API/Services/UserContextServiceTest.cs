2d756fb0f20357beaa18040cb9418cd86835b1ab
kritty04 <krit1_c@hotmail.com>
2026-03-15 22:56:27 +0800

    add User_contextservice unitest

--- HRS.Test/API/Services/UserContextServiceTest.cs
+++ HRS.Test/API/Services/UserContextServiceTest.cs
@@ -71,6 +71,23 @@ public class UserContextServiceTests
         service.GetUserId().Should().Be(expected);
     }
 
+    [Theory]
+    [InlineData("15", 15)]
+    [InlineData("0", 0)]
+    [InlineData("not-a-number", 0)]
+    [InlineData(null, 0)]
+    public void GetStoreId_ShouldParseStoreIdClaim(string? claimValue, int expected)
+    {
+        var claims = new List<Claim>();
+        if (claimValue != null)
+            claims.Add(new Claim("storeId", claimValue));
+
+        var accessor = CreateHttpContextAccessor(claims);
+        var service = new UserContextService(accessor);
+
+        service.GetStoreId().Should().Be(expected);
+    }
+
     [Fact]
     public async Task GetUserAsync_ShouldReturnUserResponseDto_WithClaims()
     {
